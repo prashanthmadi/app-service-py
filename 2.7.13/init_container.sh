1fb1b7c4a3a36dcf83c580b52710743aa5cc2986
prashanthmadi <prashanthrmadi@gmail.com>
2017-04-09 19:17:28 -0500

    testing without any conditional checks

--- 2.7.13/init_container.sh
+++ 2.7.13/init_container.sh
@@ -1,23 +1,27 @@
-exitWithMessageOnError () {
-  if [ ! $? -eq 0 ]; then
-    echo "An error has occurred during web site deployment."
-    echo $1
-    exit 1
-  fi
-}
+cd /home/site/wwwroot
+pip install -r requirements.txt
+python serve.py
 
-# 1. Run Pip Install
-if [ -e "$DEPLOYMENT_TARGET/requirements.txt" ]; then
-  cd "$DEPLOYMENT_TARGET"
-  eval pip install -r requirements.txt
-  exitWithMessageOnError "pip install failed"
-fi
-echo finished pip install
+# exitWithMessageOnError () {
+#   if [ ! $? -eq 0 ]; then
+#     echo "An error has occurred during web site deployment."
+#     echo $1
+#     exit 1
+#   fi
+# }
 
-# 2. Start Python Server
-if [ -e "$DEPLOYMENT_TARGET/serve.py" ]; then
-  cd "$DEPLOYMENT_TARGET"
-  eval python serve.py
-  exitWithMessageOnError "Python initializing file not found"
-fi
-echo app started successfully.
+# # 1. Run Pip Install
+# if [ -e "$DEPLOYMENT_TARGET/requirements.txt" ]; then
+#   cd "$DEPLOYMENT_TARGET"
+#   pip install -r requirements.txt
+#   exitWithMessageOnError "pip install failed"
+# fi
+# echo finished pip install
+
+# # 2. Start Python Server
+# if [ -e "$DEPLOYMENT_TARGET/serve.py" ]; then
+#   cd "$DEPLOYMENT_TARGET"
+#   python serve.py
+#   exitWithMessageOnError "Python initializing file not found"
+# fi
+# echo app started successfully.
