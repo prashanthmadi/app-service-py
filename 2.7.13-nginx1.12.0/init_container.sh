558eca5dea711fc31c18252cfb1d731d16f111fe
Prashanth Madi <prashanthrmadi@gmail.com>
2017-04-16 00:36:46 -0500

    :fish: added nginx 1.12.0.1

---
+++ 2.7.13-nginx1.12.0/init_container.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo Deployment Target $DEPLOYMENT_TARGET
+
+exitWithMessageOnError () {
+  if [ ! $? -eq 0 ]; then
+    echo "An error has occurred during web site deployment."
+    echo $1
+    exit 1
+  fi
+}
+
+# 1. Run Pip Install
+if [ -e "$DEPLOYMENT_TARGET/requirements.txt" ]; then
+  cd "$DEPLOYMENT_TARGET"
+  pip install -r requirements.txt
+  exitWithMessageOnError "pip install failed"
+fi
+echo finished pip install
+
+# 2. Start Python Server
+if [ -e "$DEPLOYMENT_TARGET/serve.py" ]; then
+  cd "$DEPLOYMENT_TARGET"
+  python serve.py &
+  exitWithMessageOnError "Python initializing file not found"
+fi
+echo app started successfully.
+
+nginx
+
